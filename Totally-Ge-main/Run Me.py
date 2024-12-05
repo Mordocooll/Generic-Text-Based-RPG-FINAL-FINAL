4ea1d62e321a1bee720093158398e7a8232f57a6
Albert <albertalvarez101010@gmail.com>
2024-12-05 10:54:43 -0800

    Update Run Me.py

--- Totally-Ge-main/Run Me.py
+++ Totally-Ge-main/Run Me.py
@@ -3,16 +3,16 @@ from PIL import Image, ImageTk
 import subprocess
 
 def game_start():
-    # Launch game.py and close the current window
+# Launch story1.py and close runme.py window
     subprocess.Popen(["python", "story1.py"])  # Runs game.py
-    root.destroy()  # Close the meny.py window
+    root.destroy()  
 
-# Initialize the Tkinter window
+#setting up window
 root = Tk()
 screenwidth = root.winfo_screenwidth()
 screenheight = root.winfo_screenheight() 
-# Load background image
-bg_image = Image.open("castle.png")  # Replace with your image file
+# background image
+bg_image = Image.open("castle.png") 
 bg_image = bg_image.resize((screenwidth, screenheight), Image.Resampling.LANCZOS)
 bg_photo = ImageTk.PhotoImage(bg_image)
 
@@ -21,17 +21,15 @@ canvas1 = Canvas(root, width=screenwidth, height=screenheight)
 canvas1.pack(fill="both", expand=True)
 
 # Display image
-
 canvas1.create_image(0, 0, image=bg_photo, anchor="nw")
 
-# Add Text
+# Text
 text = canvas1.create_text(screenwidth/2, screenheight/2.25, text="Generic Text Based RPG", fill="dark red", font=("Times", 30))
 bbox = canvas1.bbox(text)
 canvas1.create_rectangle(bbox, outline="black")
 
-# Add Button
+# Button that takes you to story 1
 button1 = Button(root, text="Start", command=game_start)
 button1_canvas = canvas1.create_window(screenwidth/2, screenheight/2, anchor="nw", window=button1)
 
-# Run the Tkinter event loop
 root.mainloop()
